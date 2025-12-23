061de232f23de642ea7ca4625afe11c57ff6fc56
IsExxD <ismailov.doom.2020@gmail.com>
2025-12-24 01:53:30 +0300

    калькулятор подсетей

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,4 @@
     <RouterView />
   </main>
 </template>
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   server: { port: 8080 },
@@ -8,4 +9,10 @@ export default defineConfig({
   build: { target: 'es2022' },
 
   plugins: [vue(), svgLoader()],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
